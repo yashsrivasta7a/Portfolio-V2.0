57bf165f42412c21d9aae4c45dfe06fd98eabef2
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2024-07-21 20:04:58 +0530

    portfoliov2

--- portfoliov2.0/app/page.tsx
+++ portfoliov2.0/app/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floatingnav";
 import { FaHome } from "react-icons/fa";
-import Grid from "@/components/grid";
+import Grid from "@/components/Grid";
 
 export default function Home(){
   return(
-<main className="bg-black-100 flex justify-center items-center flex-coloverflow-hidden">
-  <div className="w-full">
+    <main className=" bg-black-100">
+    <div className="max-w-7xl w-full">
    <FloatingNav
    navItems={[{
     name :"Home" , link :'/',icon:<FaHome/>}
--- portfoliov2.0/components/Grid.tsx
+++ portfoliov2.0/components/Grid.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <section id="about">
-      <BentoGrid>
-        {[
-          {title:'Title1',description:'desc1',id:1},
-          {title:'Title1',description:'desc1',id:1},
-          {title:'Title1',description:'desc1',id:1},
-        ].map((item)=>(
+    <section id="about" className="relative">
+      {/* Grid background */}
+      <div className="absolute inset-0 z-0 h-full w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.06] bg-grid-black/[0.3] flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute inset-0 pointer-events-none flex items-center justify-center dark:bg-black-100 bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      </div>
+
+      {/* Bento Grid */}
+      <BentoGrid className='relative z-10 ml-6'>
+        {gridItems.map((item) => (
           <BentoGridItem
-          id={item.id}
-          key={item.id}
-          title={item.title}
-          description={item.description}
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
           />
-        ))
-        }
+        ))}
       </BentoGrid>
     </section>
   )
